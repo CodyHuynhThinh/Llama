428fecca8d3136041e2f14195a6c6ad04f13d056
Cody <huynhdangthinh@gmail.com>
2026-04-28 00:50:15 +0700

    a

---
+++ src/main/java/com/llm/date1/Date1Application.java
@@ -0,0 +1,13 @@
+package com.llm.date1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Date1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Date1Application.class, args);
+	}
+
+}
